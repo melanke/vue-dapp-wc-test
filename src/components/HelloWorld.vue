46e01885cf8af09350b94772f7f0ae4a0b33f4f9
Gil Lopes Bueno <melanke.hc@gmail.com>
2021-10-19 15:34:22 -0300

    tests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,159 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="initCli">Init CLI</button>
+    <button @click="transferGas">Transfer Gas</button>
+    <button @click="getStream">Get Stream</button>
+    <button @click="withdrawStream">Withdraw Stream</button>
+    <button @click="multiInvoke">Multi Invoke</button>
+    <button @click="disconnect">Disconnect</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
+import { Vue } from 'vue-class-component'
+import QRCodeModal from '@walletconnect/qrcode-modal'
+import Client from '@walletconnect/client'
+import { SessionTypes } from '@walletconnect/types'
+import { WcSdk } from '@/components/WcSdk'
 
-@Options({
-  props: {
-    msg: String
-  }
-})
 export default class HelloWorld extends Vue {
-  msg!: string
+  wcClient: Client | null = null
+  session: SessionTypes.Settled | null = null
+  chainId = 'neo3:testnet'
+  scripthash = '0xd2a4cff31913016155e38e474a2c06d08be276cf'
+
+  async initCli (): Promise<void> {
+    console.log('initCli')
+    this.wcClient = await WcSdk.initClient(
+      'debug', // logger: use debug to show all log information on browser console
+      'wss://relay.walletconnect.org' // relayServer: which relay server do you want to use, alternatively you can use "wss://relay.walletconnect.org"
+    )
+
+    WcSdk.subscribeToEvents(this.wcClient, {
+      onProposal: (uri: string) => {
+        console.log('onProposal')
+        QRCodeModal.open(uri, () => { /* nothing */ })
+      }
+    })
+
+    this.session = await WcSdk.connect(this.wcClient, {
+      chainId: this.chainId, // blockchain and network identifier
+      methods: ['invokefunction', 'testInvoke', 'multiInvoke'], // which RPC methods do you plan to call
+      appMetadata: {
+        name: 'MyApplicationName', // your application name to be displayed on the wallet
+        description: 'My Application description', // description to be shown on the wallet
+        url: 'https://myapplicationdescription.app/', // url to be linked on the wallet
+        icons: ['https://myapplicationdescription.app/myappicon.png'] // icon to be shown on the wallet
+      }
+    })
+    console.log('session', this.session.state)
+    QRCodeModal.close()
+  }
+
+  async transferGas (): Promise<void> {
+    if (!this.session || !this.wcClient) {
+      window.alert('Not session or client')
+      return
+    }
+
+    console.log(this.session.state)
+
+    const senderAddress = WcSdk.getAccountAddress(this.session)
+
+    const from = { type: 'Address', value: senderAddress }
+    const recipient = { type: 'Address', value: 'NbnjKGMBJzJ6j5PHeYhjJDaQ5Vy5UYu4Fv' }
+    const value = { type: 'Integer', value: 100000000 }
+    const args = { type: 'Array', value: [] }
+
+    const resp = await WcSdk.invokeFunction(this.wcClient, this.session, this.chainId, {
+      scriptHash: this.scripthash,
+      operation: 'transfer',
+      args: [from, recipient, value, args]
+    })
+
+    console.log(resp)
+    window.alert(JSON.stringify(resp, null, 2))
+  }
+
+  async getStream (): Promise<void> {
+    if (!this.session || !this.wcClient) {
+      window.alert('Not session or client')
+      return
+    }
+    const resp = await WcSdk.testInvoke(this.wcClient, this.session, this.chainId, {
+      scriptHash: '0x010101c0775af568185025b0ce43cfaa9b990a2a',
+      operation: 'getStream',
+      args: [{ type: 'Integer', value: 2 }]
+    })
+
+    console.log(resp)
+    window.alert(JSON.stringify(resp, null, 2))
+  }
+
+  async withdrawStream (): Promise<void> {
+    if (!this.session || !this.wcClient) {
+      window.alert('Not session or client')
+      return
+    }
+
+    const streamId = { type: 'Integer', value: 2 }
+    const value = { type: 'Integer', value: 1 }
+
+    const resp = await WcSdk.invokeFunction(this.wcClient, this.session, this.chainId, {
+      scriptHash: '0x010101c0775af568185025b0ce43cfaa9b990a2a',
+      operation: 'withdraw',
+      args: [streamId, value]
+    })
+
+    console.log(resp)
+    window.alert(JSON.stringify(resp, null, 2))
+  }
+
+  async multiInvoke (): Promise<void> {
+    if (!this.session || !this.wcClient) {
+      window.alert('Not session or client')
+      return
+    }
+
+    const senderAddress = WcSdk.getAccountAddress(this.session)
+
+    const streamId = { type: 'Integer', value: 2 }
+    const value = { type: 'Integer', value: 1 }
+
+    const req1 = {
+      scriptHash: '0x010101c0775af568185025b0ce43cfaa9b990a2a',
+      operation: 'withdraw',
+      args: [streamId, value]
+    }
+
+    const from = { type: 'Address', value: senderAddress }
+    const recipient = { type: 'Address', value: 'NbnjKGMBJzJ6j5PHeYhjJDaQ5Vy5UYu4Fv' }
+    const val = { type: 'Integer', value: 100000000 }
+    const args = { type: 'Array', value: [] }
+
+    const req2 = {
+      scriptHash: this.scripthash,
+      operation: 'transfer',
+      args: [from, recipient, val, args]
+    }
+
+    const resp = await WcSdk.multiInvoke(this.wcClient, this.session, this.chainId, [req1, req2])
+
+    console.log(resp)
+    window.alert(JSON.stringify(resp, null, 2))
+  }
+
+  async disconnect (): Promise<void> {
+    if (!this.session || !this.wcClient) {
+      window.alert('Not session or client')
+      return
+    }
+
+    await WcSdk.disconnect(this.wcClient, this.session)
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
---
+++ src/components/WcSdk.ts
@@ -0,0 +1,257 @@
+import Client, { CLIENT_EVENTS } from '@walletconnect/client/dist/cjs'
+import { ERROR } from '@walletconnect/utils/dist/cjs/error'
+import { AppMetadata, PairingTypes, SessionTypes } from '@walletconnect/types/dist/cjs'
+import { RequestArguments } from '@walletconnect/jsonrpc-utils'
+
+export interface WcCallbacks {
+    onProposal?: (uri: string) => void,
+    onCreated?: (topics: string[]) => void,
+    onDeleted?: () => void
+}
+
+export interface WcConnectOptions {
+    topic?: string,
+    chainId?: string,
+    chains?: string[],
+    appMetadata: AppMetadata,
+    methods: string[]
+}
+
+export interface RpcCallResult {
+    method: string,
+    result: any,
+}
+
+export type ContractInvocation = {
+    scriptHash: string
+    operation: string
+    args: any[]
+    abortOnFail?: boolean
+}
+
+export class WcSdk {
+    wcClient?: Client
+    session?: SessionTypes.Created
+
+    async initClient (logger: string, relayServer: string) {
+      this.wcClient = await WcSdk.initClient(logger, relayServer)
+    }
+
+    subscribeToEvents (callbacks?: WcCallbacks) {
+      WcSdk.subscribeToEvents(this.wcClient, callbacks)
+    }
+
+    async loadSession () {
+      if (this.wcClient) {
+        this.session = await WcSdk.getSession(this.wcClient) || undefined
+      }
+    }
+
+    async connect (options: WcConnectOptions) {
+      if (!this.wcClient) {
+        throw Error('The client was not initialized')
+      }
+      this.session = await WcSdk.connect(this.wcClient, options)
+    }
+
+    getAccountAddress (accountIndex?: number) {
+      return this.session ? WcSdk.getAccountAddress(this.session, accountIndex) : null
+    }
+
+    get chainId () {
+      return this.session ? WcSdk.getChainId(this.session) : null
+    }
+
+    get accountAddress () {
+      return this.session ? WcSdk.getAccountAddress(this.session) : null
+    }
+
+    async disconnect () {
+      if (!this.wcClient) {
+        throw Error('The client was not initialized')
+      }
+      if (!this.session) {
+        throw Error('No session open')
+      }
+      await WcSdk.disconnect(this.wcClient, this.session)
+    }
+
+    async sendRequest (request: RequestArguments) {
+      if (!this.wcClient) {
+        throw Error('The client was not initialized')
+      }
+      if (!this.session) {
+        throw Error('No session open')
+      }
+      if (!this.chainId) {
+        throw Error('No chainId informed')
+      }
+      return await WcSdk.sendRequest(this.wcClient, this.session, this.chainId, request)
+    }
+
+    async invokeFunction (request: ContractInvocation) {
+      if (!this.wcClient) {
+        throw Error('The client was not initialized')
+      }
+      if (!this.session) {
+        throw Error('No session open')
+      }
+      if (!this.chainId) {
+        throw Error('No chainId informed')
+      }
+      return await WcSdk.invokeFunction(this.wcClient, this.session, this.chainId, request)
+    }
+
+    async testInvoke (request: ContractInvocation) {
+      if (!this.wcClient) {
+        throw Error('The client was not initialized')
+      }
+      if (!this.session) {
+        throw Error('No session open')
+      }
+      if (!this.chainId) {
+        throw Error('No chainId informed')
+      }
+      return await WcSdk.testInvoke(this.wcClient, this.session, this.chainId, request)
+    }
+
+    async multiInvoke (request: ContractInvocation[]) {
+      if (!this.wcClient) {
+        throw Error('The client was not initialized')
+      }
+      if (!this.session) {
+        throw Error('No session open')
+      }
+      if (!this.chainId) {
+        throw Error('No chainId informed')
+      }
+      return await WcSdk.multiInvoke(this.wcClient, this.session, this.chainId, request)
+    }
+
+    static async initClient (logger: string, relayProvider: string) {
+      return await Client.init({
+        logger,
+        relayProvider
+      })
+    }
+
+    static subscribeToEvents (wcClient?: Client, callbacks?: WcCallbacks) {
+      if (!wcClient) {
+        throw Error('The client was not initialized')
+      }
+
+      wcClient.on(
+        CLIENT_EVENTS.pairing.proposal,
+        async (proposal: PairingTypes.Proposal) => {
+          const { uri } = proposal.signal.params
+
+          if (callbacks && callbacks.onProposal) {
+            callbacks.onProposal(uri)
+          }
+        }
+      )
+
+      wcClient.on(CLIENT_EVENTS.pairing.created, async () => {
+        if (callbacks && callbacks.onCreated) {
+          callbacks.onCreated(wcClient.pairing.topics)
+        }
+      })
+
+      wcClient.on(CLIENT_EVENTS.session.deleted, async (session: SessionTypes.Settled) => {
+        if (session.topic !== session?.topic) return
+
+        if (callbacks && callbacks.onDeleted) {
+          callbacks.onDeleted()
+        }
+      })
+    }
+
+    static async getSession (wcClient: Client) {
+      if (wcClient?.session.topics.length) {
+        return await wcClient.session.get(wcClient.session.topics[0])
+      } else {
+        return null
+      }
+    }
+
+    static getAccountInfo (session: SessionTypes.Settled, accountIndex?: number) {
+      const index = accountIndex ?? 0
+      if (session.state.accounts.length <= index) {
+        return null
+      }
+      return session.state.accounts[index].split(':')
+    }
+
+    static getAccountAddress (session: SessionTypes.Settled, accountIndex?: number) {
+      const info = WcSdk.getAccountInfo(session, accountIndex)
+      return info && info[2]
+    }
+
+    static getChainId (session: SessionTypes.Settled, accountIndex?: number) {
+      const info = WcSdk.getAccountInfo(session, accountIndex)
+      return info && `${info[0]}:${info[1]}`
+    }
+
+    static async connect (wcClient: Client, options: WcConnectOptions) {
+      return await wcClient.connect({
+        metadata: options.appMetadata,
+        pairing: options.topic ? { topic: options.topic } : undefined,
+        permissions: {
+          blockchain: {
+            chains: options.chains ?? (options.chainId ? [options.chainId] : [])
+          },
+          jsonrpc: {
+            methods: options.methods
+          }
+        }
+      })
+    }
+
+    static async disconnect (wcClient: Client, session: SessionTypes.Created) {
+      await wcClient.disconnect({
+        topic: session.topic,
+        reason: ERROR.USER_DISCONNECTED.format()
+      })
+    }
+
+    static async sendRequest (wcClient: Client, session: SessionTypes.Created, chainId: string, request: RequestArguments): Promise<RpcCallResult> {
+      try {
+        const result = await wcClient.request({
+          topic: session.topic,
+          chainId,
+          request
+        })
+
+        return {
+          method: request.method,
+          result
+        }
+      } catch (error) {
+        return {
+          method: request.method,
+          result: { error }
+        }
+      }
+    }
+
+    static async invokeFunction (wcClient: Client, session: SessionTypes.Created, chainId: string, request: ContractInvocation) {
+      return WcSdk.sendRequest(wcClient, session, chainId, {
+        method: 'invokefunction',
+        params: [request]
+      })
+    }
+
+    static async testInvoke (wcClient: Client, session: SessionTypes.Created, chainId: string, request: ContractInvocation) {
+      return WcSdk.sendRequest(wcClient, session, chainId, {
+        method: 'testInvoke',
+        params: [request]
+      })
+    }
+
+    static async multiInvoke (wcClient: Client, session: SessionTypes.Created, chainId: string, request: ContractInvocation[]) {
+      return WcSdk.sendRequest(wcClient, session, chainId, {
+        method: 'multiInvoke',
+        params: request
+      })
+    }
+}
